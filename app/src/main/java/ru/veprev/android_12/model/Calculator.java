46b94fb6f88fe15b2ea7291da7d2c3c3a6daed10
AVYStudio <aveprev626@gmail.com>
2022-06-30 00:36:31 +0300

    added views listeners created object with calculator data land orientation

---
+++ app/src/main/java/ru/veprev/android_12/model/Calculator.java
@@ -0,0 +1,6 @@
+package ru.veprev.android_12.model;
+
+public interface Calculator {
+
+    double performOperation(double arg1, double arg2, Operator operator);
+}
---
+++ app/src/main/java/ru/veprev/android_12/model/CalculatorImplementation.java
@@ -0,0 +1,18 @@
+package ru.veprev.android_12.model;
+
+public class CalculatorImplementation implements Calculator{
+    @Override
+    public double performOperation(double arg1, double arg2, Operator operator) {
+        switch (operator) {
+            case SUM:
+                return arg1 + arg2;
+            case SUB:
+                return arg1 - arg2;
+            case MULTI:
+                return arg1 * arg2;
+            case DIV:
+                return arg1 / arg2;
+        }
+        return 0.0;
+    }
+}
